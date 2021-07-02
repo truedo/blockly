2236603731e385b9454787e7720a36d71b819f11
truedo <truedo@naver.com>
2021-07-02 12:11:52 +0900

    reset

--- src/app/commons/utils.js
+++
@@ -1,8 +0,0 @@
-global.runJS = function() {
-  Code.runJS();
-};
-
-global.enablePlayButton = function() {
-  $(".playButton").removeClass("disabled");
-  $("#runButton").css("background-image", "url(images/icons/play.svg)");
-};
